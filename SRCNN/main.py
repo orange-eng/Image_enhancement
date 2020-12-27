d1d2a1f127fc6152f64ee5d1cfc14986c58f09cd
orange <972353371@qq.com>
2020-12-27 19:16:57 +0800

    available SRCNN

---
+++ SRCNN/main.py
@@ -0,0 +1,49 @@
+import numpy as np
+import tensorflow as tf
+import pprint
+from model import SRCNN
+
+import os
+import sys
+path = os.path.abspath(os.path.dirname(sys.argv[0]))
+
+#----------------------------------------一种初始化所有参数的方法
+flags = tf.app.flags
+#第一个是参数名称，第二个参数是默认值，第三个是参数描述
+flags.DEFINE_integer("epoch",1,"Number of epoch[3]")
+flags.DEFINE_integer("batch_size",128,"the size of batch [128]")
+flags.DEFINE_integer("image_size",33,"the size of image [33]")
+flags.DEFINE_integer("label_size",21,"The size of label to produce [21]")
+flags.DEFINE_float("learning_rate", 1e-4, "The learning rate of gradient descent algorithm [1e-4]")
+flags.DEFINE_integer("c_dim", 1, "Dimension of image color. [1]")
+flags.DEFINE_integer("scale", 3, "The size of scale factor for preprocessing input image [3]")
+flags.DEFINE_integer("stride", 14, "The size of stride to apply input image [14]")
+flags.DEFINE_string("checkpoint_dir", "checkpoint", "Name of checkpoint directory [checkpoint]")
+flags.DEFINE_string("sample_dir", "sample", "Name of sample directory [sample]")
+flags.DEFINE_boolean("is_train", False, "True for training, False for testing [True]")
+FLAGS = flags.FLAGS
+
+#--------------------------------------------------------
+PP = pprint.PrettyPrinter()
+
+def main(_):
+    PP.pprint(flags.FLAGS.__flags)
+
+    if not os.path.exists(path+".\\"+FLAGS.checkpoint_dir):
+        os.makedirs(path+".\\"+FLAGS.checkpoint_dir)
+    if not os.path.exists(path+".\\"+FLAGS.sample_dir):          #如果目录中没有samle文件夹，则创建一个
+        os.makedirs(path+".\\"+FLAGS.sample_dir)
+
+    with tf.Session() as sess:
+        srcnn = SRCNN(sess = sess,
+        image_size=FLAGS.image_size,
+        label_size=FLAGS.label_size,
+        batch_size=FLAGS.batch_size,
+        c_dim=FLAGS.c_dim,
+        checkpoint_dir=FLAGS.checkpoint_dir
+        )
+        srcnn.train(FLAGS)
+
+if __name__ == '__main__':
+    tf.app.run()
+
---
+++ SRCNN/tools.py
@@ -0,0 +1,213 @@
+
+import h5py
+import numpy as np
+from imageio import imread
+import scipy.ndimage
+import glob
+
+import tensorflow as tf
+import os
+import sys
+path = os.path.abspath(os.path.dirname(sys.argv[0]))
+
+try:
+  xrange
+except:
+  xrange = range
+
+FLAGS = tf.app.flags.FLAGS
+def read_data(path):
+    """
+    Read h5 format data file
+    
+    Args:
+        path: file path of desired file
+        data: '.h5' file format that contains train data values
+        label: '.h5' file format that contains train label values
+    """
+    with h5py.File(path, 'r') as hf:
+        data = np.array(hf.get('data'))
+        label = np.array(hf.get('label'))
+        return data, label
+
+def preprocess(path, scale=3):
+    """
+    Preprocess single image file 
+        (1) Read original image as YCbCr format (and grayscale as default)
+        (2) Normalize
+        (3) Apply image file with bicubic interpolation
+
+    Args:
+        path: file path of desired file
+        input_: image applied bicubic interpolation (low-resolution)
+        label_: image with original resolution (high-resolution)
+    """
+    image = imread(path, is_grayscale=True)
+    label_ = modcrop(image, scale)
+
+    # Must be normalized
+    image = image / 255.
+    label_ = label_ / 255.
+
+    input_ = scipy.ndimage.interpolation.zoom(label_, (1./scale), prefilter=False)
+    input_ = scipy.ndimage.interpolation.zoom(input_, (scale/1.), prefilter=False)
+
+    return input_, label_
+#--------------------------------------------------------把图片的长和宽都变成scale=3的整数倍
+def modcrop(image, scale=3):
+    """
+    To scale down and up the original image, first thing to do is to have no remainder while scaling operation.
+    
+    We need to find modulo of height (and width) and scale factor.
+    Then, subtract the modulo from height (and width) of original image size.
+    There would be no remainder even after scaling operation.
+    """
+    if len(image.shape) == 3:
+        h, w, _ = image.shape
+        h = h - np.mod(h, scale)
+        w = w - np.mod(w, scale)
+        image = image[0:h, 0:w, :]
+    else:
+        h, w = image.shape
+        h = h - np.mod(h, scale)
+        w = w - np.mod(w, scale)
+        image = image[0:h, 0:w]
+    return image
+#----------------------------------------------读取图片，变为float格式
+def imread(path,is_grayscale = True):
+    """
+    Read image using its path.
+    Default value is gray-scale, and image is read by YCbCr format as the paper said.
+    """
+    if is_grayscale:
+        return scipy.misc.imread(path,flatten=True,mode='YCbCr').astype(np.float)
+    else:
+        return scipy.misc.imread(path,mode='YCbCr').astype(np.float)
+#-------------------------------------------------准备数据
+
+def prepare_data(sess, dataset):
+    """
+    Args:
+        dataset: choose train dataset or test dataset
+        
+        For train dataset, output data would be ['.../t1.bmp', '.../t2.bmp', ..., '.../t99.bmp']
+    """
+    if FLAGS.is_train:
+        filenames = os.listdir(dataset)
+        data_dir = os.path.join(os.getcwd(), dataset)
+        data = glob.glob(os.path.join(data_dir, "*.bmp"))
+    else:
+        data_dir = os.path.join(os.sep, (os.path.join(os.getcwd(), dataset)), "Set5")
+        data = glob.glob(os.path.join(data_dir, "*.bmp"))
+
+    return data
+
+def make_data(sess, data, label):
+    """
+    Make input data as h5 file format
+    Depending on 'is_train' (flag value), savepath would be changed.
+    """
+    if FLAGS.is_train:
+        savepath = os.path.join(os.getcwd(), 'checkpoint/train.h5')
+    else:
+        savepath = os.path.join(os.getcwd(), 'checkpoint/test.h5')
+
+    with h5py.File(savepath, 'w') as hf:
+        hf.create_dataset('data', data=data)
+        hf.create_dataset('label', data=label)
+
+#--------------------------------------------------
+def input_setup(sess, config):
+    """
+    Read image files and make their sub-images and saved them as a h5 file format.
+    """
+    # Load data path
+    if config.is_train:
+        data = prepare_data(sess, dataset=path+"\\Train")
+    else:
+        data = prepare_data(sess, dataset=path+"\\Test")
+
+    sub_input_sequence = []
+    sub_label_sequence = []
+    padding = abs(config.image_size - config.label_size) / 2 # 6
+
+    if config.is_train:
+        for i in xrange(len(data)):
+            input_, label_ = preprocess(data[i], config.scale)
+
+            if len(input_.shape) == 3:
+                h, w, _ = input_.shape
+            else:
+                h, w = input_.shape
+
+            for x in range(0, h-config.image_size+1, config.stride):
+                for y in range(0, w-config.image_size+1, config.stride):
+                    sub_input = input_[x:x+config.image_size, y:y+config.image_size] # [33 x 33]
+                    sub_label = label_[x+int(padding):x+int(padding)+config.label_size, y+int(padding):y+int(padding)+config.label_size] # [21 x 21]
+
+                    # Make channel value
+                    sub_input = sub_input.reshape([config.image_size, config.image_size, 1])  
+                    sub_label = sub_label.reshape([config.label_size, config.label_size, 1])
+
+                    sub_input_sequence.append(sub_input)
+                    sub_label_sequence.append(sub_label)
+
+    else:
+        input_, label_ = preprocess(data[2], config.scale)
+
+        if len(input_.shape) == 3:
+            h, w, _ = input_.shape
+        else:
+            h, w = input_.shape
+
+        # Numbers of sub-images in height and width of image are needed to compute merge operation.
+        nx = ny = 0 
+        for x in range(0, h-config.image_size+1, config.stride):
+            nx += 1; ny = 0
+            for y in range(0, w-config.image_size+1, config.stride):
+                ny += 1
+                sub_input = input_[x:x+config.image_size, y:y+config.image_size] # [33 x 33]
+                sub_label = label_[x+int(padding):x+int(padding)+config.label_size, y+int(padding):y+int(padding)+config.label_size] # [21 x 21]
+                
+                sub_input = sub_input.reshape([config.image_size, config.image_size, 1])  
+                sub_label = sub_label.reshape([config.label_size, config.label_size, 1])
+
+                sub_input_sequence.append(sub_input)
+                sub_label_sequence.append(sub_label)
+
+    """
+    len(sub_input_sequence) : the number of sub_input (33 x 33 x ch) in one image
+    (sub_input_sequence[0]).shape : (33, 33, 1)
+    """
+    # Make list to numpy array. With this transform
+    arrdata = np.asarray(sub_input_sequence) # [?, 33, 33, 1]
+    arrlabel = np.asarray(sub_label_sequence) # [?, 21, 21, 1]
+
+    make_data(sess, arrdata, arrlabel)
+
+    if not config.is_train:
+        return nx, ny
+
+#--------------------------------------------------存储图像
+
+def imsave(image,path):
+    return scipy.misc.imsave(path,image)
+# #--------------------------------------------------
+# '''作用是什么。不太清楚'''
+def merge(images,size):
+    h,w = images.shape[1],images.shape[2]
+    img = np.zeros((h*size[0],w*size[1],1))
+    for idx,image in enumerate(images):
+        i = idx % size[1]
+        # j = idx % size[1]
+        j = idx // size[1]
+        img[j*h:j*h+h,i*w:i*w+w,:] = image
+    return img
+
+
+
+
+
+
+
+
